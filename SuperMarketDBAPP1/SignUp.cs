103843312c6af598c006c646a2132c6a10a48ba6
Marawan Ashraf <161965435+Marooo25@users.noreply.github.com>
2025-05-13 16:28:08 +0300

    Add files via upload

---
+++ SuperMarketDBAPP1/SignUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperMarketDBAPP1
+{
+    public partial class SignUp : Form
+    {
+
+        // Connection Sentence
+        static String sql = "Data Source=MARO25;Initial Catalog=DataBase1;Integrated Security=True;Encrypt=False;Trust Server Certificate=True";
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void SignUp_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2ImageButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
